2fd004b6f8f359f0915eef5aa4e23dc4e2edaa06
Trần Trung Dũng <dungtt1@emg.vn>
2025-07-11 15:46:49 +0700

    Initial Commit

---
+++ EMGWAY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EMGWAY
+//
+//  Created by Mr. Dũng on 11/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EMGWAYTests/EMGWAYTests.swift
@@ -0,0 +1,17 @@
+//
+//  EMGWAYTests.swift
+//  EMGWAYTests
+//
+//  Created by Mr. Dũng on 11/7/25.
+//
+
+import Testing
+@testable import EMGWAY
+
+struct EMGWAYTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
